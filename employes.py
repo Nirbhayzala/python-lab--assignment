10f7feadb3b0654e913fed8af390f9af302137cf
Nirbhayzala <135641475+Nirbhayzala@users.noreply.github.com>
2024-07-13 18:37:08 +0530

    Add files via upload

---
+++ employes.py
@@ -0,0 +1,13 @@
+EMPNAME = []
+
+EMPNAME. append("nirbhay")
+EMPNAME. append("mandip")
+EMPNAME.append("ronak")
+
+print("Employee Names after additions:", EMPNAME)
+EMPNAME.remove("mandip")
+
+print("Employee Names after removal  :", EMPNAME)
+EMPNAME.append("nishant")
+
+print("Final Employee Names :", EMPNAME)
